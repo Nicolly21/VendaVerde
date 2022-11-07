5bfbdd6345f74429dc0c6fa3fd97dc09fe29b99e
Nicolly de Oliveira Santos <n.de.oliveira.santos@avanade.com>
2022-11-07 12:35:09 -0300

    Criação da tela Meu carrinho e adaptação.

---
+++ VendaVerde/Migrations/20221107140928_AddCarrinho.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VendaVerde.Migrations
+{
+    public partial class AddCarrinho : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItensCarrinho",
+                columns: table => new
+                {
+                    ItemCarrinhoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantidade = table.Column<int>(nullable: false),
+                    CarrinhoId = table.Column<string>(nullable: true),
+                    ProdutoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensCarrinho", x => x.ItemCarrinhoId);
+                    table.ForeignKey(
+                        name: "FK_ItensCarrinho_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensCarrinho_ProdutoId",
+                table: "ItensCarrinho",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensCarrinho");
+        }
+    }
+}
