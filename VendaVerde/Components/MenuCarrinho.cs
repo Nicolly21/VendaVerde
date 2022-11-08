aa7dadd2737cc11d52bbaaebc6dd1e70a1bb7227
Nicolly de Oliveira Santos <n.de.oliveira.santos@avanade.com>
2022-11-08 15:44:51 -0300

    Criação MenuCarrinho e MenuCategoria

---
+++ VendaVerde/Components/MenuCarrinho.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendaVerde.Models;
+using VendaVerde.ViewModels;
+
+namespace VendaVerde.Components
+{
+    public class MenuCarrinho : ViewComponent
+    {
+        private readonly Carrinho _carrinho;
+
+        public MenuCarrinho(Carrinho carrinho)
+        {
+            _carrinho = carrinho;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinho.GetShoppingCartItems();
+            _carrinho.ItensCarrinho = itens;
+
+            var carrinhoViewModel = new CarrinhoViewModel
+            {
+                Carrinho = _carrinho,
+                TotalItensCarrinho = _carrinho.GetShoppingCartTotal()
+            };
+
+            return View(carrinhoViewModel);
+        }
+    }
+}
---
+++ VendaVerde/Components/MenuCategoria.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendaVerde.Models.Interfaces;
+
+namespace VendaVerde.Components
+{
+    public class MenuCategoria : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public MenuCategoria(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.AllCategorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
